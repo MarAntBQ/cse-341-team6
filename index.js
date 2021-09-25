ff2db3fb727b9c39cbe38b53cd165ec2771f782f
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-09-24 21:09:14 -0500

    Agregando codigo

--- index.js
+++ index.js
@@ -35,9 +35,18 @@ app
   //.set('view engine', 'hbs')
   .use(bodyParser.urlencoded({ extended: false })) // For parsing the body of a POST
   .use('/ta01', ta01Routes)
-  .use('/ta02', ta02Routes)
+ // .use('/ta02', ta02Routes)
   .use('/ta03', ta03Routes)
   .use('/ta04', ta04Routes)
+
+  .get('/ta02', (req, res, next) => {
+    // This is the primary index, always handled last.
+    res.render('pages/ta02', {
+      title: 'TA02',
+      path: '/',
+    });
+  })
+
   .get('/', (req, res, next) => {
     // This is the primary index, always handled last.
     res.render('pages/index', {
@@ -45,6 +54,7 @@ app
       path: '/',
     });
   })
+
   .use((req, res, next) => {
     // 404 page
     res.render('pages/404', { title: '404 - Page Not Found', path: req.url });
