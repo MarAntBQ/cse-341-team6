9ad2fc0dc653b6b671c98b07babb58500956ebf1
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-10-01 23:25:09 -0500

    New Changes, trying to push in the Hosting
    Not read the JSON File!!

--- week3-tw/index.js
+++ week3-tw/index.js
@@ -13,12 +13,12 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const jsonReader = require('./models/jsonReader');
+const Informations = require('./models/jsonReader');
 
 app.get('/', (req, res, next) => {
-    jsonReader.fetchAll((data) => {
-        res.render('index', {
-            lists: data,
+    Informations.fetchAll((informations) => {
+        res.render('home', {
+            lists: informations,
             pageTitle: 'Home',
             path: '/'
         });
