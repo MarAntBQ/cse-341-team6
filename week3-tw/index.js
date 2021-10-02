4d6257d58cb757251e95a1bbb8b1bbfd4a46b6b0
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-10-01 21:27:59 -0500

    Add new styles!!!

--- week3-tw/index.js
+++ week3-tw/index.js
@@ -26,7 +26,7 @@ app.get('/', (req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not Found!', SiteName: GeneralAppName, Navpath: 'Not Found'});
+    res.status(404).render('404', {pageTitle: 'Page not Found!'});
 });
 
 app.listen(3000);
